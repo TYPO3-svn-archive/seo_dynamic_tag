def9a1243d00e046bdc7818ba2fdc04c0d5de12b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-30 13:06:33 +0000

    1.2.0 +++ #49442
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seo_dynamic_tag/trunk@78696 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -6,7 +6,7 @@ if( ! defined ( 'TYPO3_MODE' ) )
 
   //  add static TypoScript
 t3lib_extMgm::addStaticFile( $_EXTKEY, 'static/',                           'SEO Dynamic Tag'                           );
-t3lib_extMgm::addStaticFile( $_EXTKEY, 'static/cal/eventViewYearMonthDay/', '+SEO Dynamic Tag cal event view'           );
+t3lib_extMgm::addStaticFile( $_EXTKEY, 'static/cal/eventView/',             '+SEO Dynamic Tag cal event view'           );
 //t3lib_extMgm::addStaticFile( $_EXTKEY, 'static/cal/eventViewYearMonthDay/', '+SEO Dynamic Tag cal event view (default)' );
 //t3lib_extMgm::addStaticFile( $_EXTKEY, 'static/cal/eventViewGetdate/',      '+SEO Dynamic Tag cal event view (getdate)' );
 t3lib_extMgm::addStaticFile( $_EXTKEY, 'static/tt_news/',                   '+SEO Dynamic Tag tt_news'                  );

944c6de158da4ab2d1d13e7d9604eee56e1a11b9
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-10-07 18:07:55 +0000

    6.0.0 +++ #i0014
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/seo_dynamic_tag/trunk@86516 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,21 +1,11 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "seo_dynamic_tag".
- *
- * Auto generated 18-09-2013 00:42
- *
- * Manual updates:
- * Only the data in the array - everything else is removed by next
- * writing. "version" and "dependencies" must not be touched!
- ***************************************************************/
-
 $EM_CONF[$_EXTKEY] = array(
 	'title' => 'SEO dynamic tag 2',
 	'description' => 'Search Engine Optimization (SEO) for the title tag, the canonical tag and the meta tags author, description and keywords.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '2.4.2',
+	'version' => '2.4.3',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -32,18 +22,15 @@ $EM_CONF[$_EXTKEY] = array(
 	'author_company' => '',
 	'CGLcompliance' => '',
 	'CGLcompliance_note' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '4.5.0-6.2.99',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"9acd";s:12:"ext_icon.gif";s:4:"43fc";s:17:"ext_localconf.php";s:4:"6850";s:14:"ext_tables.php";s:4:"7c97";s:14:"doc/manual.pdf";s:4:"8ad4";s:14:"doc/manual.sxw";s:4:"a874";s:34:"pi1/class.tx_seodynamictag_pi1.php";s:4:"0a1c";s:20:"res/realurl_conf.php";s:4:"cc98";s:20:"static/constants.txt";s:4:"8813";s:16:"static/setup.txt";s:4:"3bbd";s:24:"static/cal/constants.txt";s:4:"e575";s:41:"static/cal/eventViewGetdate/constants.txt";s:4:"430b";s:46:"static/cal/eventViewYearMonthDay/constants.txt";s:4:"e200";s:28:"static/tt_news/constants.txt";s:4:"4644";s:32:"static/tt_products/constants.txt";s:4:"12bd";s:28:"static/tt_products/setup.txt";s:4:"e05e";}',
-	'suggests' => array(
-	),
+  'constraints' => array(
+    'depends' => array(
+      'typo3' => '4.5.0-6.2.99',
+    ),
+    'conflicts' => array(
+    ),
+    'suggests' => array(
+    ),
+  ),
 );
 
 ?>
